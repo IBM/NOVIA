dfb439f14feb45518bacfcbe0f90afb83ecdd4bd
David Trilla <dtrilla@ac.upc.edu>
2020-01-18 18:39:21 +0100

    Added baseline benchmark

---
+++ fusion/benchmarks/env.mk
@@ -0,0 +1,8 @@
+GXX := g++
+CFLAGS := -O0
+
+LLVM_ROOT := ../../llvm-project/build/bin/
+LLVM := $(LLVM_ROOT)llvm
+CLANG := $(LLVM_ROOT)clang
+CLANGXX := $(LLVM_ROOT)clang++
+CLANG_FLAGS := -emit-llvm -c
