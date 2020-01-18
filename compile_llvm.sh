7f2c9b6272c0e51933f1514d7db3af79403fa1e6
David Trilla <dtrilla@ac.upc.edu>
2020-01-18 19:28:48 +0100

    fixed compile scripts

--- compile_llvm.sh
+++ compile_llvm.sh
@@ -4,5 +4,5 @@ source env.sh
 cd $LLVM_TOP
 mkdir -p build
 cd build
-cmake -G "Unix Makefiles" -DLLVM_ENABLE_PROJECTS="clang;libcxx;libcxxabi" -DCMAKE_INSTALL_PREFIX=$pwd -DLLVM_ENABLE_ASSERTIONS=ON -DCMAKE_BUILD_TYPE=Release ../llvm
+cmake -G "Unix Makefiles" -DLLVM_ENABLE_PROJECTS="clang;libcxx;libcxxabi" -DCMAKE_INSTALL_PREFIX=$LLVM_TOP/build/ -DLLVM_ENABLE_ASSERTIONS=ON -DCMAKE_BUILD_TYPE=Release ../llvm
 make -j$NUM_THREADS install
--- env.sh
+++ env.sh
@@ -1,1 +1,3 @@
 export LLVM_TOP=./llvm-project/
+export LLVM_ROOT=./llvm-project/build/
+export NUM_THREADS=16
