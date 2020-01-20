aa6291d28a2155da94ff91c4de3cf271fb272c6e
David Trilla <dtrilla@ac.upc.edu>
2020-01-20 15:11:12 +0100

    finished porting

--- env.sh
+++ env.sh
@@ -1,3 +1,5 @@
-export LLVM_TOP=$(pwd)/llvm-project/
-export LLVM_ROOT=$(pwd)/llvm-project/build/
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+export LLVM_TOP=$DIR/llvm-project/
+export LLVM_ROOT=$DIR/llvm-project/build/
 export NUM_THREADS=16
