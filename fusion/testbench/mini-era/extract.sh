df5c45ca34f6d573ca37d3ed1b1609a4f33754f1
David Trilla <david.trilla.rodriguez3@ibm.com>
2020-03-31 08:51:50 -0400

    changed names of test bbbs and fixed testbench script

--- fusion/testbench/mini-era/extract.sh
+++ fusion/testbench/mini-era/extract.sh
@@ -4,7 +4,9 @@ BENCH_DIR=~/git/mini-era/
 
 ${LLVM_ROOT}/bin/opt -load ${PASSES_DIR}libfusionlib.so -renameBBs < ${BENCH_DIR}main.bc  > main_rn.bc
 ${LLVM_ROOT}/bin/opt -load ${PASSES_DIR}libfusionlib.so -mergeBBList -bbs bblist.txt < main_rn.bc  > out.bc
-${LLVM_ROOT}/bin/opt -load ${PASSES_DIR}libfusionlib.so -memoryFootprint < out.bc > out2.bc
+#${LLVM_ROOT}/bin/opt -load ${PASSES_DIR}libfusionlib.so -memoryFootprint < out.bc > out2.bc
 ${LLVM_ROOT}/bin/clang++ -O0 -ggdb -lpython3.6m out.bc
+${LLVM_ROOT}/bin/llvm-dis out.bc 
+${LLVM_ROOT}/bin/llvm-objdump -d a.out > dis.txt
 
 
