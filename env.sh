9e465ea886ac68a0c63b845cbdcefc6a29fb37f8
David Trilla <david.trilla@ibm.com>
2020-04-25 12:42:24 -0400

    Added graph capabilities to the tool

--- env.sh
+++ env.sh
@@ -1,5 +1,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+export GRAPHVIZ_TOP=$DIR/graphviz/
+export GRAPHVIZ_ROOT=$DIR/graphviz/build/
+
 export LLVM_TOP=$DIR/llvm-project/
 export LLVM_ROOT=$DIR/llvm-project/build/
 export NUM_THREADS=1
