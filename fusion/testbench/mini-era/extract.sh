35362f5a268331b0b6c5747b961e1f667de5d770
David Trilla <david.trilla@ibm.com>
2020-05-15 21:00:53 -0400

    cleand up extract scripts

--- fusion/testbench/mini-era/extract.sh
+++ fusion/testbench/mini-era/extract.sh
@@ -1,12 +1,9 @@
 source ../../../env.sh
 PASSES_DIR=../../build/src/
-BENCH_DIR=~/git/mini-era/
+BENCH_DIR=../../../../mini-era/
 
-${LLVM_ROOT}/bin/opt -load ${PASSES_DIR}libfusionlib.so -renameBBs < ${BENCH_DIR}main.bc  > main_rn.bc
-${LLVM_ROOT}/bin/opt -load ${PASSES_DIR}libfusionlib.so -mergeBBList -bbs bblist.txt --graph_dir imgs < main_rn.bc  > out.bc
-#${LLVM_ROOT}/bin/opt -load ${PASSES_DIR}libfusionlib.so -memoryFootprint < out.bc > out2.bc
+${LLVM_ROOT}/bin/opt -load ${PASSES_DIR}libfusionlib.so -mergeBBList -bbs bblist.txt --graph_dir imgs < ${BENCH_DIR}main_rn.bc  > out.bc
+${LLVM_ROOT}/bin/llvm-dis out.bc
 ${LLVM_ROOT}/bin/clang++ -O0 -ggdb -lpython3.6m out.bc
-${LLVM_ROOT}/bin/llvm-dis out.bc 
-${LLVM_ROOT}/bin/llvm-objdump -d a.out > dis.txt
 
 
--- fusion/testbench/multidep/extract.sh
+++ fusion/testbench/multidep/extract.sh
@@ -3,6 +3,6 @@ PASSES_DIR=../../build/src/
 BENCH_DIR=../../benchmarks/multidep/
 
 ${LLVM_ROOT}/bin/opt -load ${PASSES_DIR}libfusionlib.so -renameBBs < ${BENCH_DIR}multidep.bc  > ${BENCH_DIR}multidep_rn.bc
-${LLVM_ROOT}/bin/opt -load ${PASSES_DIR}libfusionlib.so -mergeBBList -bbs bblist.txt < ${BENCH_DIR}multidep_rn.bc  > out.bc
+${LLVM_ROOT}/bin/opt -load ${PASSES_DIR}libfusionlib.so -mergeBBList -bbs bblist.txt --graph_dir imgs < ${BENCH_DIR}multidep_rn.bc  > out.bc
 ${LLVM_ROOT}/bin/clang++ out.bc
 ${LLVM_ROOT}/bin/llvm-dis out.bc
