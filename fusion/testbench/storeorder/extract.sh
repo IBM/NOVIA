9281a86c23d3f2bd8438195124e796c766080a25
David Trilla <david.trilla@ibm.com>
2020-05-15 21:04:02 -0400

    added testbench for store order

---
+++ fusion/testbench/storeorder/extract.sh
@@ -0,0 +1,8 @@
+source ../../../env.sh
+PASSES_DIR=../../build/src/
+BENCH_DIR=../../benchmarks/storeorder/
+
+${LLVM_ROOT}/bin/opt -load ${PASSES_DIR}libfusionlib.so -renameBBs < ${BENCH_DIR}storeorder.bc  > ${BENCH_DIR}storeorder_rn.bc
+${LLVM_ROOT}/bin/opt -load ${PASSES_DIR}libfusionlib.so -mergeBBList -bbs bblist.txt --graph_dir imgs < ${BENCH_DIR}storeorder_rn.bc  > out.bc
+${LLVM_ROOT}/bin/clang++ out.bc
+${LLVM_ROOT}/bin/llvm-dis out.bc
