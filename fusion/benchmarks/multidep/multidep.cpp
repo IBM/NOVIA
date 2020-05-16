bb843ba040b0c40299b0a01ed917bbf0800ee38a
David Trilla <david.trilla@ibm.com>
2020-05-15 21:02:35 -0400

    iupdate

--- fusion/benchmarks/multidep/multidep.cpp
+++ fusion/benchmarks/multidep/multidep.cpp
@@ -10,10 +10,10 @@ int main(int argn, char **argc){
 
 	int a = atoi(argc[1]) ;
 	if (a%2) {
-    a = (a/2)*5;
+    a = (a/2)*5*(5+5);
 	}
 	else {
-		a = ((a+2)%10)*5;
+		a = ((a+2)%10)*(5+5);
 	}
 	cout << "Result: " << a << endl;
 	return 0;
