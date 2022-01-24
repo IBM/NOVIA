2e56eec37567d3ef275dfee399874746007aaf76
David Trilla <david.trilla@ibm.com>
2022-01-24 16:15:40 -0500

    fixed install

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo $DIR
+exit
+
 source $DIR/../env.sh
 
 git clone -b llvmorg-13.0.0 https://github.com/llvm/llvm-project $LLVM_TOP 
-git clone -b 93d330be8 https://gitlab.com/graphviz/graphviz $GRAPHVIZ_TOP 
+git clone -b 93d330be https://gitlab.com/graphviz/graphviz $GRAPHVIZ_TOP 
 
-$DIR/compile_llvm.sh
-$DIR/compile_graphviz.sh
+$DIR/scripts/compile_llvm.sh
+$DIR/scripts/compile_graphviz.sh
 
 cd $DIR/../fusion
 mkdir -p build
