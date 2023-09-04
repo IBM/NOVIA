9522586e4db2ebef76bd051c6ffd03f561c12f82
David Trilla <david.trilla@ibm.com>
2023-09-03 20:44:49 -0400

    fixing patches

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-source $DIR/../env.sh
+BASEDIR=$DIR/../
+source $BASEDIR/env.sh
 
 git clone -b llvmorg-13.0.0 https://github.com/llvm/llvm-project $LLVM_TOP 
 cd $LLVM_TOP
-patch -s -p0 < $DIR/patches/llvm.patch
+git apply $BASEDIR/patches/llvm.patch
 cd ../
 
 git clone  https://gitlab.com/graphviz/graphviz $GRAPHVIZ_TOP 
@@ -15,19 +16,19 @@ cd ../
 git clone https://github.com/openhwgroup/cva6.git $ARIANE_TOP 
 cd $ARIANE_TOP
 git checkout 6000e32b
-patch -s -p0 < $DIR/patches/ariane.patch
+git apply $BASEDIR/patches/ariane.patch
 cd ../
 
-git clone https://github.ibm.com/NOVIA/riscv-binutils.git $RVBINUTILS_TOP
+git clone https://github.com/riscv/riscv-binutils-gdb.git $RVBINUTILS_TOP
 cd $RVBINUTILS_TOP
 git checkout f356740
-patch -s -p0 < $DIR/patches/binutils.patch
+git apply $BASEDIR/patches/binutils.patch
 cd ../
 
-$DIR/scripts/compile_graphviz.sh
-$DIR/scripts/compile_llvm_minimal.sh
+$BASEDIR/scripts/compile_graphviz.sh
+$BASEDIR/scripts/compile_llvm_minimal.sh
 
-cd $DIR/fusion
+cd $BASEDIR/fusion
 mkdir -p build
 cd build
 cmake ../
